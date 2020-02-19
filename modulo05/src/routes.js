dea295ea2bd367eee35a3eb9bd89cc56e1625c80
Denner Rondinely <denner.rondinely@gmail.com>
2020-02-19 06:47:40 -0300

    Criar rotas e estilos globais

---
+++ modulo05/src/routes.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from 'react';
+import Main from './pages/Main';
+import Repository from './pages/Repository';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/repository" exact component={Repository} />
+        <Route path="/" component={Main} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
